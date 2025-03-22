a46d9aad9d6a2688899add9b0b188641257235f8
di-iwan <iwan.lelikov@gmail.com>
2025-03-23 00:07:35 +0300

    лого и дарк тем

--- src/lib/components/navbar/navbar.svelte
+++ src/lib/components/navbar/navbar.svelte
@@ -2,15 +2,19 @@
 	import Button from "../ui/button/button.svelte";
 	import NavMenu from "./nav-menu.svelte";
 	import NavigationSheet from "./navigation-sheet.svelte";
+    import Logo from "../images/logo.svelte";
+	import ThemeSwitch from "./theme-switch.svelte";
 </script>
 
 <header class="sticky my-6 top-6 min h-16 bg-background border dark:border-slate-700/70 max-w-screen-xl mx-auto rounded-full">
     <nav class="h-full flex items-center justify-between mx-auto px-4">
-        <NavMenu class="hidden md:block" />
+        <Logo class="h-2/3 dark:fill-white" />
+        <NavMenu class="max-md:hidden" />
         <div class="flex items-center gap-3">
             <Button class="rounded-full">
                 Вход
             </Button>
+            <ThemeSwitch class="rounded-full max-md:hidden" />
             <div class="md:hidden">
                 <NavigationSheet />
             </div>
